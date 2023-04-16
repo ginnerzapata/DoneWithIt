dbe3a332d69a794f09451f95f678aaff137e2de1
Ginner Zapata <gzapata@vizzn.ca>
2023-04-16 04:11:21 -0300

    finished ViewImageScreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,8 +1,46 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import colors from "../config/colors";
 
 export default function ViewImageScreen() {
-  return <Image source={require("../assets/chair.jpg")} />;
+  return (
+    <View styles={styles.container}>
+      <View style={styles.closeIcon} />
+      <View style={styles.deleteIcon} />
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+    zIndex: 3,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+    zIndex: 3,
+  },
+  container: {
+    backgroundColor: colors.black,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
