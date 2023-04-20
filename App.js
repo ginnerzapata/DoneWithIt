0643d4f289107303868cf43d2bad4d2e379b1864
Ginner Zapata <gzapata@vizzn.ca>
2023-04-20 10:09:05 -0300

    basic listing components

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import React from "react";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingDetailsScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,22 @@
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
 
-export default function WelcomeScreen() {
+function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Dont Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -21,18 +25,11 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
-
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
   },
   logo: {
     width: 100,
@@ -43,4 +40,11 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
 });
+
+export default WelcomeScreen;
